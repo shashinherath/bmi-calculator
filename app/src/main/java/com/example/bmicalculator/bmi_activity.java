cb19239b612b8a49aa32944510c8ed9f01b21a77
ShashinHerath <116209315+ShashinHerath@users.noreply.github.com>
2024-03-16 15:44:50 +0530

    Update bmi_activity.java

--- app/src/main/java/com/example/bmicalculator/bmi_activity.java
+++ app/src/main/java/com/example/bmicalculator/bmi_activity.java
@@ -2,13 +2,28 @@ package com.example.bmicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class bmi_activity extends AppCompatActivity {
+
+    android.widget.Button recalculateBmi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
+        getSupportActionBar().hide();
+        recalculateBmi =  findViewById(R.id.recalculatebmi);
+
+        recalculateBmi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(bmi_activity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
