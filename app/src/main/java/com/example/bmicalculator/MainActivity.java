e209923b1bc94c0d0b2d8628415ce1a5d49f80aa
ShashinHerath <116209315+ShashinHerath@users.noreply.github.com>
2024-03-19 22:24:29 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -6,10 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     android.widget.Button calculateBmi;
+
+    TextView currentHeight;
+    TextView currentAge, currentWeight;
+    ImageView incrementAge, incrementWeight, decrementAge, decrementWeight;
+    SeekBar seekBarForHeight;
+    RelativeLayout male, female;
+
+    int weight;
+    int age = 22;
+    int currentProgress;
+    String minProgress = "170";
+    String typeOfUser = "0";
+    String weight2 = "55";
+    String age2 = "22";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +36,16 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         calculateBmi = findViewById(R.id.calculatebmi);
+        currentAge = findViewById(R.id.currentage);
+        currentWeight = findViewById(R.id.currentweight);
+        currentHeight = findViewById(R.id.currentheight);
+        incrementAge = findViewById(R.id.incrementage);
+        decrementAge = findViewById(R.id.decrementage);
+        incrementWeight = findViewById(R.id.incrementweight);
+        decrementWeight = findViewById(R.id.decrementweight);
+        male = findViewById(R.id.male);
+        female = findViewById(R.id.female);
+
 
         calculateBmi.setOnClickListener(new View.OnClickListener() {
             @Override
